ca95ab37c7ca28ce093a8b19c6b36c249f1ccbe2
song.li <song.li@silverera.net>
2020-01-22 18:10:40 +0800

    adjsut to new API.

--- qtrpc.cpp
+++ qtrpc.cpp
@@ -4,163 +4,161 @@
 //
 #include "qtrpc.h"
 
+namespace trpc {
 
-namespace trpc
-{
+void QtRpcClient::connectServer(QString ip, int port, SocketCb cb) {
+  close();
 
-    void QtRpcClient::connectServer(QString ip, int port, SocketCb cb)
-    {
-        close();
+  socket = new QTcpSocket();
+  mIsConnected = false;
 
-        socket = new QTcpSocket();
-        mIsConnected = false;
+  socket->connect(socket, &QTcpSocket::connected, [this, cb] {
+    mIsConnected = true;
+    output.device()->reset();
+    cb(true, QTcpSocket::UnknownSocketError);
+  });
 
-        socket->connect(socket, &QTcpSocket::connected, [this,cb] {
-            mIsConnected = true;
-            output.device()->reset();
-            cb(true, QTcpSocket::UnknownSocketError);
-        });
+  socket->connect(
+      socket,
+      QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),
+      [this, cb](QAbstractSocket::SocketError err) {
+        socketError = err;
+        cb(false, err);
+      });
 
-        socket->connect(socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error), [this, cb](QAbstractSocket::SocketError err) {
-            socketError = err;
-            cb(false, err);
-        });
+  socket->connect(socket, &QAbstractSocket::disconnected,
+                  [this] { mIsConnected = false; });
 
-        socket->connect(socket, &QAbstractSocket::disconnected, [this] {
-            mIsConnected = false;
-        });
+  socket->connect(socket, &QTcpSocket::readyRead, [this] {
+    QDataStream input{socket};
+    while (socket) {
+      if (packageSize == 0 && socket->bytesAvailable() >= sizeof(packageSize)) {
+        input >> packageSize;  // read block size.
+      }
+      if (packageSize > 0 && socket->bytesAvailable() >= packageSize) {
+        QDataStream s(socket->read(packageSize));
+        onReceive(s);
+        assert(s.status() == QDataStream::Ok);
 
-        socket->connect(socket, &QTcpSocket::readyRead, [this] {
-            QDataStream input{ socket };
-            while (socket) {
-                if ( packageSize == 0 && socket->bytesAvailable() >= sizeof(packageSize) ) {
-                    input >> packageSize;   // read block size.
-                }
-                if ( packageSize > 0 && socket->bytesAvailable() >= packageSize ) {
-                    QDataStream s(socket->read(packageSize));
-                    onReceive(s);
-                    assert(s.status() == QDataStream::Ok);
-
-                    if (onRead) onRead(packageSize);
-                    packageSize = 0;                    
-                }
-                else {
-                    break;
-                }
-            }
-        });
-
-        send = [this]() {
-            int sz = block.size();
-            QByteArray b;
-            QDataStream s(&b, QIODevice::WriteOnly);
-            s << sz;
-            socket->write(b);
-            socket->write(block);
-            socket->flush(); // 可能触发disconnect
-            block.clear();
-
-            if (mIsConnected) {                
-                output.device()->reset();
-            }            
-        };
-
-        socket->connectToHost(ip, port);
+        if (onRead)
+          onRead(packageSize);
+        packageSize = 0;
+      } else {
+        break;
+      }
     }
+  });
 
-    void QtRpcClient::close()
-    {
-        if (socket) {
-            socket->close();
-            delete socket;
-            socket = nullptr;
-        }        
-        mIsConnected = false;
+  flush = [this]() {
+    int sz = block.size();
+    QByteArray b;
+    QDataStream s(&b, QIODevice::WriteOnly);
+    s << sz;
+    socket->write(b);
+    socket->write(block);
+    socket->flush();  // 可能触发disconnect
+    block.clear();
+
+    if (mIsConnected) {
+      output.device()->reset();
     }
+  };
 
-    QtRpcServer::~QtRpcServer()
-    {
-        close();
-    }
-
-    void QtRpcServer::close()
-    {
-        if (socket) {
-            socket->close();
-            delete socket;
-            socket = nullptr;
-        }
-    }
-
-    void QtRpcServer::startListen(QString ip, int port, SocketCb cb)
-    {        
-        socket = new QTcpServer();
-
-        socket->connect(socket, &QTcpServer::acceptError, [this, cb](QAbstractSocket::SocketError err) {
-            cb(false, err);
-        });
-
-        socket->connect(socket, &QTcpServer::newConnection, [this] {
-
-            auto& session = sessions[sessionID];
-            session.sid = sessionID++;
-            session.client = socket->nextPendingConnection();
-            addSession(session.sid, session.output);
-            // set build-in session info
-            setSessionField(session.sid, "host", session.client->localAddress().toString());
-
-            socket->connect(session.client, &QTcpSocket::readyRead, [this, &session] {
-                QDataStream input(session.client);                
-                while ( session.client->bytesAvailable() ) {
-                    if ( !session.packageSize && session.client->bytesAvailable() >= sizeof(session.packageSize) ) {
-                        input >> session.packageSize;
-                    }
-                    if ( session.packageSize && session.client->bytesAvailable() >= session.packageSize ) {
-                        QDataStream s(session.client->read(session.packageSize));
-                        auto sid_copy = session.sid;
-                        onReceive(session.sid, s);
-                        assert(s.status() == QDataStream::Ok);
-
-                        if (onRead) onRead(session.packageSize);
-                        session.packageSize = 0;
-                        if (sessions.find(sid_copy) == sessions.end()) return;
-                    }
-                    else {
-                        break;
-                    }
-                }
-            });
-
-            socket->connect(session.client, &QAbstractSocket::disconnected, [this, &session] {
-                removeSession(session.sid);
-                sessions.erase(session.sid);
-            });
-        });
-
-        send = [this](SessionID sid) {
-            if ( sessions.find(sid) == sessions.end() ) return;
-
-            auto& session = sessions[sid];
-            int sz = session.block.size();
-            QByteArray b;
-            QDataStream s(&b, QIODevice::WriteOnly);
-            s << sz;
-            session.client->write(b);
-            session.client->write(session.block);
-            session.client->flush(); // 可能触发disconnect 
-            if (sessions.find(sid) == sessions.end()) return;
-
-            session.block.clear();
-            session.output.device()->reset();
-        };
-
-        initHandlers();
-
-        if (!socket->listen(QHostAddress(ip), port)) {
-            return cb(false, socket->serverError());
-        }
-
-        cb(true, QTcpSocket::UnknownSocketError);
-    }
+  socket->connectToHost(ip, port);
 }
 
+void QtRpcClient::close() {
+  if (socket) {
+    socket->close();
+    delete socket;
+    socket = nullptr;
+  }
+  mIsConnected = false;
+}
+
+QtRpcServer::~QtRpcServer() {
+  close();
+}
+
+void QtRpcServer::close() {
+  if (socket) {
+    socket->close();
+    delete socket;
+    socket = nullptr;
+  }
+}
+
+void QtRpcServer::startListen(QString ip, int port, SocketCb cb) {
+  socket = new QTcpServer();
+
+  socket->connect(
+      socket, &QTcpServer::acceptError,
+      [this, cb](QAbstractSocket::SocketError err) { cb(false, err); });
+
+  socket->connect(socket, &QTcpServer::newConnection, [this] {
+    auto& session = sessions[sessionID];
+    session.sid = sessionID++;
+    session.client = socket->nextPendingConnection();
+    addSession(session.sid, session.output);
+    // set build-in session info
+    setSessionField(session.sid, "host",
+                    session.client->localAddress().toString());
+
+    socket->connect(session.client, &QTcpSocket::readyRead, [this, &session] {
+      QDataStream input(session.client);
+      while (session.client->bytesAvailable()) {
+        if (!session.packageSize &&
+            session.client->bytesAvailable() >= sizeof(session.packageSize)) {
+          input >> session.packageSize;
+        }
+        if (session.packageSize &&
+            session.client->bytesAvailable() >= session.packageSize) {
+          QDataStream s(session.client->read(session.packageSize));
+          auto sid_copy = session.sid;
+          onReceive(session.sid, s);
+          assert(s.status() == QDataStream::Ok);
+
+          if (onRead)
+            onRead(session.packageSize);
+          session.packageSize = 0;
+          if (sessions.find(sid_copy) == sessions.end())
+            return;
+        } else {
+          break;
+        }
+      }
+    });
+
+    socket->connect(session.client, &QAbstractSocket::disconnected,
+                    [this, &session] {
+                      removeSession(session.sid);
+                      sessions.erase(session.sid);
+                    });
+  });
+
+  flush = [this](SessionID sid) {
+    if (sessions.find(sid) == sessions.end())
+      return;
+
+    auto& session = sessions[sid];
+    int sz = session.block.size();
+    QByteArray b;
+    QDataStream s(&b, QIODevice::WriteOnly);
+    s << sz;
+    session.client->write(b);
+    session.client->write(session.block);
+    session.client->flush();  // 可能触发disconnect
+    if (sessions.find(sid) == sessions.end())
+      return;
+
+    session.block.clear();
+    session.output.device()->reset();
+  };
+
+  if (!socket->listen(QHostAddress(ip), port)) {
+    return cb(false, socket->serverError());
+  }
+
+  cb(true, QTcpSocket::UnknownSocketError);
+}
+}  // namespace trpc
--- qtrpc.h
+++ qtrpc.h
@@ -8,84 +8,81 @@
 #include <QtNetwork/QtNetwork>
 #include "trpc.h"
 
-
-namespace trpc
-{
-    inline QDataStream& operator >> (QDataStream& s, std::string& v)
-    {
-        char* p;
-        s >> p;
-        v = p;
-        delete[]p;
-        return s;
-    }
-
-    inline QDataStream& operator<< (QDataStream& s, std::string& v)
-    {
-        return s << v.c_str();
-    }
-
-    using SocketCb = std::function<void(bool, QAbstractSocket::SocketError)>;
-
-    class QtRpcClient :public RpcClient<QDataStream>
-    {
-    public:
-        QtRpcClient():  RpcClient(output){}
-        ~QtRpcClient() { close(); }
-        
-        void connectServer(QString ip, int port, SocketCb cb);
-        bool isConnected() { return mIsConnected; }
-        QAbstractSocket::SocketError getSocketError() { return socketError; }
-        void close();        
-        QTcpSocket* getSocket() { return socket; }
-        function<void(int)> onRead;
-    private:
-        bool mIsConnected = false;
-        QAbstractSocket::SocketError socketError;
-        int packageSize = 0;
-        QTcpSocket* socket = nullptr;
-        QByteArray  block;
-        QDataStream output{ &block, QIODevice::WriteOnly };        
-    };
-
-
-    class QtRpcServer : public RpcServer<QDataStream>
-    {
-    public:
-        ~QtRpcServer();
-        void close();
-        void startListen(QString addr, int port, SocketCb cb);        
-        QVariant getSessionField(int sid, QString k) { return sessions[sid].data[k]; }
-        void setSessionField(int sid, QString k, QVariant v) { sessions[sid].data[k] = v; }
-        function<void(int)> onRead;
-    private:
-        struct Session
-        {
-            int sid = 0;
-            QTcpSocket* client;
-            QByteArray  block;
-            QDataStream output{ &block, QIODevice::WriteOnly };
-            int packageSize = 0;
-            map<QString, QVariant> data;
-        };
-        QTcpServer* socket = nullptr;
-        map<int, Session> sessions;
-        int sessionID = 100;
-    };
-
-    
-
-    template<typename T>
-    class QtRpcHandler : public RpcHandler<T, QDataStream>
-    {
-    public:
-        using RpcHandler<T,QDataStream>::RpcHandler;
-
-        QtRpcHandler(string name): RpcHandler(name){}
-        
-        QtRpcServer* getServer() {  return static_cast<QtRpcServer*>(server); }
-        QVariant getSessionField(SessionID sid, QString f) {  return getServer()->getSessionField(sid, f);  }
-        void setSessionField(int sid, QString k, QVariant v) { getServer()->setSessionField(sid, k, v); }
-    };
-
+namespace trpc {
+inline QDataStream& operator>>(QDataStream& s, std::string& v) {
+  char* p;
+  s >> p;
+  v = p;
+  delete[] p;
+  return s;
 }
+
+inline QDataStream& operator<<(QDataStream& s, std::string& v) {
+  return s << v.c_str();
+}
+
+using SocketCb = std::function<void(bool, QAbstractSocket::SocketError)>;
+
+class QtRpcClient : public RpcClient<QDataStream> {
+ public:
+  QtRpcClient() : RpcClient(output) {}
+  ~QtRpcClient() { close(); }
+
+  void connectServer(QString ip, int port, SocketCb cb);
+  bool isConnected() { return mIsConnected; }
+  QAbstractSocket::SocketError getSocketError() { return socketError; }
+  void close();
+  QTcpSocket* getSocket() { return socket; }
+  function<void(int)> onRead;
+
+ private:
+  bool mIsConnected = false;
+  QAbstractSocket::SocketError socketError;
+  int packageSize = 0;
+  QTcpSocket* socket = nullptr;
+  QByteArray block;
+  QDataStream output{&block, QIODevice::WriteOnly};
+};
+
+class QtRpcServer : public RpcServer<QDataStream> {
+ public:
+  ~QtRpcServer();
+  void close();
+  void startListen(QString addr, int port, SocketCb cb);
+  QVariant getSessionField(int sid, QString k) { return sessions[sid].data[k]; }
+  void setSessionField(int sid, QString k, QVariant v) {
+    sessions[sid].data[k] = v;
+  }
+  function<void(int)> onRead;
+
+ private:
+  struct Session {
+    int sid = 0;
+    QTcpSocket* client;
+    QByteArray block;
+    QDataStream output{&block, QIODevice::WriteOnly};
+    int packageSize = 0;
+    map<QString, QVariant> data;
+  };
+  QTcpServer* socket = nullptr;
+  map<int, Session> sessions;
+  int sessionID = 100;
+};
+
+template <typename T>
+class QtRpcHandler : public RpcHandler<T, QDataStream> {
+ public:
+  using RpcHandler<T, QDataStream>::RpcHandler;
+
+  QtRpcHandler(string name) : RpcHandler(name) {}
+
+  QtRpcServer* getServer() { return static_cast<QtRpcServer*>(server); }
+  QVariant getSessionField(SessionID sid, QString f) {
+    return getServer()->getSessionField(sid, f);
+  }
+  void setSessionField(int sid, QString k, QVariant v) {
+    getServer()->setSessionField(sid, k, v);
+  }
+};
+
+}  // namespace trpc
